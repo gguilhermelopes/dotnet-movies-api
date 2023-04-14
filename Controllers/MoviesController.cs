cecfe6899dffe8e1012c9b9fb5df0cb304999aed
gguilhermelopes <guilhermelopes275@gmail.com>
2023-04-14 15:54:23 -0300

    feat: set httppost method to create movie in database

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -19,16 +19,29 @@ public class MoviesController : ControllerBase
   public ActionResult<IEnumerable<Movie>> Get()
   {
     var movies = _context.Movies?.ToList();
-    if (movies is null) return NotFound("Couldn't find movies.");
+    if (movies is null)
+      return NotFound("Couldn't find movies.");
     return movies;
   }
 
-  [HttpGet("{id:int}")]
+  [HttpGet("{id:int}", Name = "GetMovie")]
 
   public ActionResult<Movie> Get(int id)
   {
     var movie = _context.Movies?.FirstOrDefault(p => p.MovieId == id);
-    if (movie is null) return NotFound("Couldn't find refered movie.");
+    if (movie is null)
+      return NotFound("Couldn't find refered movie.");
     return movie;
+  }
+
+  [HttpPost]
+  public ActionResult Post(Movie movie)
+  {
+    if (movie is null)
+      return BadRequest();
+    _context.Movies?.Add(movie);
+    _context.SaveChanges();
+
+    return new CreatedAtRouteResult("GetMovie", new { id = movie.MovieId }, movie);
   }
 }
