54ac4463b67c5a2be8dd7fb8c17aa2ab5a5937c1
gguilhermelopes <guilhermelopes275@gmail.com>
2023-04-14 01:26:46 -0300

    feat: set relationship between entities

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -1,8 +1,15 @@
+using System.Collections.ObjectModel;
+
 namespace MoviesApi.Models;
 
 public class Genre
 {
+  public Genre()
+  {
+    Movies = new Collection<Movie>();
+  }
   public int GenreId { get; set; }
   public string? Name { get; set; }
   public string? ImageURI { get; set; }
+  public ICollection<Movie>? Movies { get; set; }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,8 +6,9 @@ public class Movie
   public string? Name { get; set; }
   public int Year { get; set; }
   public string? Sinopsis { get; set; }
-  public string? Genre { get; set; }
   public string? Director { get; set; }
-  public string? ImageURI { get; private set; }
+  public string? ImageURI { get; set; }
   public DateTime EntryDate { get; set; }
+  public int GenreId { get; set; }
+  public Genre? Genre { get; set; }
 }
