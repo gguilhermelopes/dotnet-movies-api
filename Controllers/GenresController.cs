fa909e68373a4dc9ca07978cbbc9027461b16e00
gguilhermelopes <guilhermelopes275@gmail.com>
2023-04-17 14:18:30 -0300

    refactor: deal with errors with try-catch

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -19,60 +19,104 @@ public class GenresController : ControllerBase
   [HttpGet]
   public ActionResult<IEnumerable<Genre>> Get()
   {
-    var genres = _context.Genres?.AsNoTracking().ToList();
-    if (genres is null)
-      return NotFound("Couldn't find any genres.");
-    return genres;
+    try
+    {
+      var genres = _context.Genres?.AsNoTracking().ToList();
+      if (genres is null)
+        return NotFound("Couldn't find any genres.");
+      return genres;
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpGet("{id:int}", Name = "GetGenre")]
   public ActionResult<Genre> Get(int id)
   {
-    var genre = _context.Genres?.AsNoTracking().FirstOrDefault(g => g.GenreId == id);
-    if (genre is null)
-      return NotFound("Couldn't find refered genre.");
-    return genre;
+    try
+    {
+      var genre = _context.Genres?.AsNoTracking().FirstOrDefault(g => g.GenreId == id);
+      if (genre is null)
+        return NotFound("Couldn't find refered genre.");
+      return genre;
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpGet("movies")]
   public ActionResult<IEnumerable<Genre>> GetGenresMovies()
   {
-    return _context.Genres?.AsNoTracking().Include(g => g.Movies).ToList();
+    try
+    {
+
+      return _context.Genres?.AsNoTracking().Include(g => g.Movies).ToList();
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpPost]
   public ActionResult Post(Genre genre)
   {
-    if (genre is null)
-      return BadRequest();
-    _context.Genres?.Add(genre);
-    _context.SaveChanges();
+    try
+    {
+      if (genre is null)
+        return BadRequest();
 
-    return new CreatedAtRouteResult("GetGenre", new { id = genre.GenreId }, genre);
+      _context.Genres?.Add(genre);
+      _context.SaveChanges();
+
+      return new CreatedAtRouteResult("GetGenre", new { id = genre.GenreId }, genre);
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpPut("{id:int}")]
   public ActionResult Put(int id, Genre genre)
   {
-    if (id != genre.GenreId)
-      return BadRequest();
+    try
+    {
+      if (id != genre.GenreId)
+        return BadRequest();
 
-    _context.Entry(genre).State = EntityState.Modified;
-    _context.SaveChanges();
+      _context.Entry(genre).State = EntityState.Modified;
+      _context.SaveChanges();
 
-    return Ok(genre);
+      return Ok(genre);
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpDelete("{id:int}")]
   public ActionResult Delete(int id)
   {
-    var genre = _context.Genres?.FirstOrDefault(g => g.GenreId == id);
-    if (genre is null)
-      return NotFound("Couldn't find refered genre.");
+    try
+    {
+      var genre = _context.Genres?.FirstOrDefault(g => g.GenreId == id);
+      if (genre is null)
+        return NotFound("Couldn't find refered genre.");
 
-    _context.Genres?.Remove(genre);
-    _context.SaveChanges();
+      _context.Genres?.Remove(genre);
+      _context.SaveChanges();
 
-    return Ok($"Genre {genre.Name} deleted.");
+      return Ok($"Genre {genre.Name} deleted.");
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 }
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -19,56 +19,92 @@ public class MoviesController : ControllerBase
   [HttpGet]
   public ActionResult<IEnumerable<Movie>> Get()
   {
-    var movies = _context.Movies?.AsNoTracking().ToList();
-    if (movies is null)
-      return NotFound("Couldn't find movies.");
-    return movies;
+    try
+    {
+      var movies = _context.Movies?.AsNoTracking().ToList();
+      if (movies is null)
+        return NotFound("Couldn't find movies.");
+      return movies;
+    }
+    catch (System.Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpGet("{id:int}", Name = "GetMovie")]
   public ActionResult<Movie> Get(int id)
   {
-    var movie = _context.Movies?.AsNoTracking().FirstOrDefault(m => m.MovieId == id);
-    if (movie is null)
-      return NotFound("Couldn't find refered movie.");
-    return movie;
+    try
+    {
+      var movie = _context.Movies?.AsNoTracking().FirstOrDefault(m => m.MovieId == id);
+      if (movie is null)
+        return NotFound("Couldn't find refered movie.");
+      return movie;
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpPost]
   public ActionResult Post(Movie movie)
   {
-    if (movie is null)
-      return BadRequest();
-    _context.Movies?.Add(movie);
-    _context.SaveChanges();
+    try
+    {
+      if (movie is null)
+        return BadRequest();
+      _context.Movies?.Add(movie);
+      _context.SaveChanges();
 
-    return new CreatedAtRouteResult("GetMovie", new { id = movie.MovieId }, movie);
+      return new CreatedAtRouteResult("GetMovie", new { id = movie.MovieId }, movie);
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpPut("{id:int}")]
 
   public ActionResult Put(int id, Movie movie)
   {
-    if (id != movie.MovieId)
-      return BadRequest();
+    try
+    {
+      if (id != movie.MovieId)
+        return BadRequest();
 
-    _context.Entry(movie).State = EntityState.Modified;
-    _context.SaveChanges();
+      _context.Entry(movie).State = EntityState.Modified;
+      _context.SaveChanges();
 
-    return Ok(movie);
+      return Ok(movie);
+
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 
   [HttpDelete("{id:int}")]
   public ActionResult Delete(int id)
   {
-    var movie = _context.Movies?.FirstOrDefault(m => m.MovieId == id);
+    try
+    {
+      var movie = _context.Movies?.FirstOrDefault(m => m.MovieId == id);
 
-    if (movie is null)
-      return NotFound("Couldn't find refered movie.");
+      if (movie is null)
+        return NotFound("Couldn't find refered movie.");
 
-    _context.Movies?.Remove(movie);
-    _context.SaveChanges();
+      _context.Movies?.Remove(movie);
+      _context.SaveChanges();
 
-    return Ok($"{movie.Name} deleted.");
+      return Ok($"{movie.Name} deleted.");
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "Oops! It looks like something went wrong on our end. Our team has been notified and we are working to resolve the issue as soon as possible. Please try again later.");
+    }
   }
 }
